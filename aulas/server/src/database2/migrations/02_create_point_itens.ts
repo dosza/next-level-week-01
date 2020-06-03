7b363bee819b4851e007fe68a5887d892f048863
Daniel <oliveira.daniel109@gmail.com>
2020-06-02 23:32:18 -0300

    remove invalid database file
    init database directory

---
+++ aulas/server/src/database2/migrations/02_create_point_itens.ts
@@ -0,0 +1,12 @@
+import Knex from 'knex'
+export async function  up (knex:Knex){
+    return knex.schema.createTable('point_itens', table =>{
+        table.increments('id').primary();
+        table.integer('id_ponto').notNullable().references('id').inTable('points');
+        table.integer('id_itens').notNullable().references('id').inTable('itens');
+    });
+}
+
+export async function down (knex:Knex){
+    return knex.schema.dropTable('points_itens');
+}
