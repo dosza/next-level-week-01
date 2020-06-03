18181e2cae1eae29d90f53e42a5847a3be9e6ecf
Daniel <oliveira.daniel109@gmail.com>
2020-06-03 18:49:35 -0300

    Adiciona  novas rotas
    Adiciona rota para listar todos os pontos, 
    adiciona rota para listar um ponto especifico

--- aulas/server/src/controllers/PointsController.ts
+++ aulas/server/src/controllers/PointsController.ts
@@ -48,6 +48,45 @@ class PointsController{
         });
     }
 
+    async show(request:Request, response:Response){
+        const {id} = request.params;
+        const point = await knex('points').where('id',id).first();
+
+        if (!point){
+            return response.status(400).json({ message: 'Point not found'});
+        }
+        /*equivalente select  * from itens 
+            join point_itens ON itens.id = point_itens.id_item
+            where point_itens.id =  {id}
+            select(itens.title)
+        */
+
+        const itens = await knex('itens').join('point_itens','itens.id','=','point_itens.id_item')
+        .where('point_itens.id_ponto',id).select('itens.title');
+
+        return response.json({point,itens}) ;
+    }
+
+    async index(request:Request, response:Response){
+        //cidade, uf, itens ]
+        const {city,uf,itens} = request.query;
+        const parsedItens = String(itens).split(',').map(item => Number(item.trim()));
+
+        const points = await knex('points')
+        .join('point_itens', 'points.id','=','point_itens.id_ponto')
+        .whereIn('point_itens.id_item ',parsedItens)
+        .where('city', String(city))
+        .where('uf',String(uf))
+        .distinct() 
+        .select('points.*');
+        
+
+        console.log(city,uf,itens);
+        return response.json(points);
+         
+
+    }
+
 }
 
 export default PointsController;
--- aulas/server/src/routes.ts
+++ aulas/server/src/routes.ts
@@ -7,8 +7,9 @@ const pointController = new PointsController();
 const itensController = new ItensController();
 
 //rota padrão
-routes.get('/itens',itensController.index);
+routes.get ('/itens',itensController.index);
 routes.post('/points', pointController.create);
+routes.get ('/points', pointController.index);
 
-
+routes.get('/points/:id', pointController.show);
 export default routes;
