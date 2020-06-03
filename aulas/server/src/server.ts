2f187e75e39450761d2c32543c7570e4b658f68a
Daniel <oliveira.daniel109@gmail.com>
2020-06-03 00:26:01 -0300

    Add /uploads
    Add images  
    Update database.sqlite3 with seed!

--- aulas/server/src/server.ts
+++ aulas/server/src/server.ts
@@ -66,9 +66,11 @@ app.listen(3333);
  */
 import express from 'express';
 import routes  from './routes';
+import path from 'path';
 const app = express();
 app.use(express.json());
 app.use(routes);
-app.listen(3333);
+app.use('/uploads',express.static( path.resolve(__dirname,'..','uploads') ) );
+app.listen(3333);   
 
 
