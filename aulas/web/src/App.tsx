a129273393ed1c066e9bdc8f37a24006ef5cf436
Daniel <oliveira.daniel109@gmail.com>
2020-06-04 04:47:50 -0300

    Frontend: página de cadastro
    Cria as páginas: Create-Point e Home,
    Adiciona navegação entre elas

--- aulas/web/src/App.tsx
+++ aulas/web/src/App.tsx
@@ -1,23 +1,11 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
-import  Header from './Header'; 
+import Routes from './routes';
 
 //JSX : Sintaxe  dentro do XML do Javascript
 function App() {
-  let [counter,setCounter] = useState(0); //
-
-  function handlerButton(){
-  //  console.log(counter);
-    setCounter(counter + 1);
-  }
-
   return (
-    <div>
-      <Header title="Hello Word"/> 
-        <h1>{counter}</h1>
-        <button type="button" onClick={handlerButton}>Aumentar </button>
-
-    </div>
+    <Routes/>
   );
 }
 
