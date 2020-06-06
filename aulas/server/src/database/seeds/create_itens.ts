576558c37a14a3ae4d36b956585a1d12d1946856
Daniel <oliveira.daniel109@gmail.com>
2020-06-06 02:15:46 -0300

    fix database.sql
    corrige 'baterias.svg' para 'bateria.svg' na tabela itens

--- aulas/server/src/database/seeds/create_itens.ts
+++ aulas/server/src/database/seeds/create_itens.ts
@@ -3,7 +3,7 @@ import Knex from 'knex';
 export async function seed(knex:Knex){
     return await knex('itens').insert( [
         {title: "Lâmpadas", image: "lampadas.svg"},
-        {title: "Pilhas e baterias", image: "baterias.svg"},
+        {title: "Pilhas e baterias", image: "bateria.svg"},
         {title: "Papéis e Papelão", image: "papeis-papelao.svg"},
         {title: "Resíduos Eletrônicos", image: "eletronicos.svg"},
         {title: "Resíduos Orgânicos", image: "organicos.svg"},
