bc6c6f06cb9629ff3c254d72ab5d989a9547e3bd
DanielOliveiraSouza <oliveira.daniel109@gmail.com>
2021-05-10 20:19:57 -0300

    aviso CVS 10/05/2021

--- aulas/update-sources.sh
+++ aulas/update-sources.sh
@@ -5,9 +5,17 @@ dirs=(
 	"mobile"
 )
 start_dir=$PWD
+
+ARG="$1"
 for i in ${!dirs[*]}
 do
 	cd  ${dirs[i]}
+	if [ "$ARG" != "" ]; then 
+		cat yarn.lock | grep  $ARG
+		if [ $? = 0 ]; then
+			yarn upgrade $ARG
+		fi
+	fi
 	yarn upgrade
 	cd ..
 done
